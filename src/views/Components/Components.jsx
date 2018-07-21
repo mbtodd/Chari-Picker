c9bbde92eb9b75c45c62de716dc34dd83137c102
Michael Todd <michaelbtodd@yahoo.com>
2018-07-21 15:43:42 -0400

    css and jsx changes

--- src/views/Components/Components.jsx
+++ src/views/Components/Components.jsx
@@ -69,14 +69,14 @@ class Components extends React.Component {
 
         <div className={classNames(classes.main, classes.mainRaised)}>
           <SectionBasics />
-          <SectionNavbars />
+          {/* <SectionNavbars />
           <SectionTabs />
           <SectionPills />
           <SectionNotifications />
           <SectionTypography />
           <SectionJavascript />
           <SectionCarousel />
-          <SectionCompletedExamples />
+          <SectionCompletedExamples /> */}
           <SectionLogin />
           <GridItem md={12} className={classes.textCenter}>
             <Link to={"/login-page"} className={classes.link}>
